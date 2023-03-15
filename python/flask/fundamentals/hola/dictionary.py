af712f6dd22df88190b21f13e562311519be60f2
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-15 10:23:55 -0300

    1.6

---
+++ python/flask/fundamentals/hola/dictionary.py
@@ -0,0 +1,7 @@
+findesemana = {"Dom": "Domingo", "Sáb": "Sábado"} # notación literal
+capitales = {} # crea un diccionario vacío y luego agrega valores
+capitales["svk"] = "Bratislava"
+capitales["deu"] = "Berlin"
+capitales["dnk"] = "Copenhagen"
+capitales["clp"] = "Santiago"
+print(capitales ["deu"])
