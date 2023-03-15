135e4ff3857ec358e71a10e955d5b4bf1b0c8b0c
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-15 12:51:30 -0300

    the end?

--- python/flask/fundamentals/evaluaciones/bucle_for_básico1.py
+++ python/flask/fundamentals/evaluaciones/bucle_for_básico1.py
@@ -1,15 +1,12 @@
 #First Loop (0 to 151)
-
 for x in range(0,151):
     print(x)
 
 #Loop number two (5 to 1000)
-
 for x in range(0,1000,5):
     print(x)
 
 #Loop to "Dojo's Way" (0 to 100)
-
 for x in range(0,100):
     if x % 10 == 0:
         print("Coding Dojo")
@@ -18,7 +15,6 @@ for x in range(0,100):
         print(x)
 
 #Loop "Whoa is a big Fool" (0 to 500)
-
 y = 0
 for x in range(0,500000):
     if x % 2 != 0:
@@ -30,7 +26,6 @@ for x in range(2018,0,-4):
     print(x)
 
 #Loop with var
-
 lowNum = 0
 highNum = 20
 Mult = 5
