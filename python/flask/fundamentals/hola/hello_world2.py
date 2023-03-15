605e307036e9a3d5d2d2f7ecb0126358ed811ef7
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-15 08:53:18 -0300

    1.0

--- python/flask/fundamentals/hola/hello_world2.py
+++ python/flask/fundamentals/hola/hello_world2.py
@@ -1,17 +1,21 @@
+#Print Hello World
 print("Hello World")
 
+#Name var 1
 name = "ignacio"
 
 print ("Hello",name)
 print ("Hello"+ name)
 
+#Name var 2
 name = 42
 
 print ("Hi",str(name))
 print ("Hi",str(name))
 
+#Favorite Food
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 
-print ("I love to eat")
-print ("I love to eat")
+print ("I love to eat",)
+print ("I love to eat",)
