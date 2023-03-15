557901e47e200c5dbcbfb67c73586e67717ad8e0
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-15 08:50:40 -0300

    source

--- python/flask/fundamentals/hola/hello_world2.py
+++ python/flask/fundamentals/hola/hello_world2.py
@@ -2,21 +2,16 @@ print("Hello World")
 
 name = "ignacio"
 
-print ("Hello, name")
-print ("Hello + name")
+print ("Hello",name)
+print ("Hello"+ name)
 
 name = 42
 
-print ("Hi",str("name"))
-print ("Hi",str("name"))
+print ("Hi",str(name))
+print ("Hi",str(name))
 
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 
-print ("")
-print ("")
-
-hw = ""
-py = ""
-
-print = ("")
+print ("I love to eat")
+print ("I love to eat")
