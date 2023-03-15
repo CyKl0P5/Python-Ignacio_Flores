2c9c2952ca7e4a1dccbae6388fe7a62e342501db
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-15 09:11:16 -0300

    1.4

--- python/flask/fundamentals/hola/hello_world2.py
+++ python/flask/fundamentals/hola/hello_world2.py
@@ -21,4 +21,31 @@ fave_food2 = "pizza"
 print (f'I love to eat {fave_food1}')
 print (f'I love to eat {fave_food2}')
 
-#Another ways to say "Hello World"
+#Another ways to say "Hello World" lmao
+
+#First Way
+saying_hello = "hello world"
+busy_tosay_hello = "bye world"
+
+
+print (f'{saying_hello}')
+print (f'{busy_tosay_hello}')
+
+
+#Plan B: Randomizer
+
+import random
+
+saludos = ['Hello_World', 'Hello World', 'Hola Mundo', 'Hallo Welt']
+sal = random.choice(saludos)
+
+print(f'Translating... {sal}')
+
+if sal == 'Hallo Welt':
+        print('Successfully Traduced to German')
+elif(sal == 'Hola Mundo'):
+        print('Successfully Traduced to Spanish')
+else:
+        print('Error 404')
+
+
