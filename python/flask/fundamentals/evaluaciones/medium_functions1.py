c85cd10e4a492663447bbf3b63694fc5913463e6
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-20 11:07:53 -0300

    gorillaz

--- python/flask/fundamentals/evaluaciones/medium_functions1.py
+++ python/flask/fundamentals/evaluaciones/medium_functions1.py
@@ -1,11 +1,11 @@
-x = [ [5,2,3], [10,8,9] ] 
-estudiantes = [
-    {'first_name':  'Michael', 'last_name' : 'Jordan'},
-    {'first_name' : 'John', 'last_name' : 'Rosales'}
-]
-directorio_deportes = {
-    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
-}
-z = [ {'x': 10, 'y': 20} ]
+#1 hacer una update en listas y diccionarios
 
+#1.1 cambiar el valor 10 en "x" a 15. Una vez terminado, x deberia ser "[ [5,2,3], [15,8,9] ]"
+x = [ [5,2,3], [10,8,9]]
+
+changevalue = x[1][0]
+print(changevalue)
+x[1][0] = 15
+print(x)
+
+#1.2 cambiar "apellido" del primer alumno de "jordan a Bryant"
