4baa2d5558a509682b2301ba5a1a8aa9c00597d5
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-20 10:46:05 -0300

    the strokes

--- python/flask/fundamentals/evaluaciones/functions_for_basic2.py
+++ python/flask/fundamentals/evaluaciones/functions_for_basic2.py
@@ -9,4 +9,10 @@ print(countdown(5))
 #2 Imprimir y devolver
 def print_and_return(list):
     print(list[0])
-    return(print)
+    return list[1]
+print(print_and_return([1,2]))
+
+#3 Primero Más Longitud
+def primero_mas_longitud(list):
+    return list[0] + len(list)
+print(primero_mas_longitud([1,2,3,4,5]))
