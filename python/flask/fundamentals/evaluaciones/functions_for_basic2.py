7e7d8b7a309af7177a5e004bbfa108e78c1b8462
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-20 09:28:11 -0300

    test1

---
+++ python/flask/fundamentals/evaluaciones/functions_for_basic2.py
@@ -0,0 +1,10 @@
+#1 cuenta regresiva
+def add(a):
+    b = []
+    y = v[1]
+    for u in v:
+        b.append(u)
+        if len(b) <=2:
+            return False
+        return b
+    print(mas([5,4,3,2,1,0]))
