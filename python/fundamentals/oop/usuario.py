09d330dc6da0e264e6fe72930b6148066974361b
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-27 13:04:09 -0300

    paramore

---
+++ python/fundamentals/oop/usuario.py
@@ -0,0 +1,13 @@
+class user:
+    name_bank ="bancoestado"
+    def _init_(self,name, email_address) -> None:
+        self.name = name
+        self.email = email.address
+        self.balance_cuenta = 0
+name = 'ignacio'
+email = 'nacho@gmail.com'
+guido = user(name,email)
+name = 'hayley'
+email = 'hayleywilliams@gmail.com'
+monty = user(name,email )
+
