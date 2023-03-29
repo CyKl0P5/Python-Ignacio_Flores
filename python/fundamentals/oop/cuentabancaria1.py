2e53aecf681e0b6b022e3425e6f5572112c36ac3
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-29 09:36:31 -0300

    paramore

--- python/fundamentals/oop/cuentabancaria1.py
+++ python/fundamentals/oop/cuentabancaria1.py
@@ -1,3 +1,11 @@
 class BankAcc:
     def _init_(self, tasa_interes, balance):
-        
+
+
+
+#user class
+def deposito(self, amount):
+
+def mostrar_info_cuenta(self):
+
+def generar_interes(self):
