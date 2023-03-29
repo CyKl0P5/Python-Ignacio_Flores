e727c852ffb1c09dee27cef59024d29543d7941d
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-03-29 08:51:54 -0300

    paramore

--- python/fundamentals/oop/encadenar.py
+++ python/fundamentals/oop/encadenar.py
@@ -35,5 +35,4 @@ nobbles.make_deposit(1000).make_deposit(1000).make_withdrawl(500).make_withdrawl
 
 bob.make_deposit(1500).make_withdrawl(1000).make_withdrawl(5000).make_withdrawl(3000).display_user_balance()
 
-
 nobbles.transfer_money(400, nacho)
