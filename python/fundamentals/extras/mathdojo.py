1b6f93a06c4fdd712cc4b629cabe3b3d3aaa4ecb
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-10 10:33:08 -0400

    ¿u

--- python/fundamentals/extras/mathdojo.py
+++ python/fundamentals/extras/mathdojo.py
@@ -2,4 +2,11 @@ class MathDojo:
     def __init__(self):
         self.result = 0
     def add(self, num, *nums):
-        
+        # sample
+    def subtract(self, num, *nums):
+        # sample
+#
+md = MathDojo()
+#
+x = md.add(2).add(2,5,1).substract(3,2).result
+print(x)
