2276e6a57b6d6226ff6fcd995efe0d7592c25848
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-12 09:07:24 -0400

    black sheep

---
+++ python/fundamentals/extras/listas.py
@@ -0,0 +1,16 @@
+class = SNode:
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+    def add_to_front(self, val):
+        new_node = SNode(val)
+
+
+class = SList:
+    def __init__(self):
+        self.head = None
+
+    def add_to_front(self,val):
+        new_node = SNode(val)
+
