5a57491987174e2960220d65b6278c64158c3974
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-12 09:16:40 -0400

    aaaaaaaaaa

--- python/fundamentals/extras/listas.py
+++ python/fundamentals/extras/listas.py
@@ -1,16 +1,19 @@
-class = SNode:
-    def __init__(self, val):
-        self.value = val
-        self.next = None
+class SList:
+    def __init__(self):
+        self.head = None
 
     def add_to_front(self, val):
         new_node = SNode(val)
 
 
-class = SList:
+class SNode:
     def __init__(self):
         self.head = None
 
     def add_to_front(self,val):
-        new_node = SNode(val)
+        new_node = SList(val)
+        current_head = self.head
+        new_node.next = current_head
+        self.head = new_node
+        return self
 
