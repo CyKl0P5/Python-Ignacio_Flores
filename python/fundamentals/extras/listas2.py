96daa1f0ff9887f107f3896fa71251010ac08293
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-17 10:14:19 -0400

    anjfionds

--- python/fundamentals/extras/listas2.py
+++ python/fundamentals/extras/listas2.py
@@ -8,3 +8,20 @@ class Elist:
     def __init__(self):
         self.root = None
 
+    def ilv(self, data): 
+        if self.root == None:
+            new_node = Node(data)
+            self.root = new_node
+        else: 
+            print
+        return self
+
+
+    def atf(self, data):
+        if self.root == None:
+            self.ilv(data)
+            return
+        else:
+            new_node = Node(data)
+            new_node.next = self.root
+            self.root
