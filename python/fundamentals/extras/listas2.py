c4a54298f5e5b64d95291faddef562845c00ad7f
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-17 10:19:32 -0400

    room of mirrors

--- python/fundamentals/extras/listas2.py
+++ python/fundamentals/extras/listas2.py
@@ -24,4 +24,11 @@ class Elist:
         else:
             new_node = Node(data)
             new_node.next = self.root
-            self.root
+            self.root.prev = new_node
+            self.root = new_node
+        return self
+
+    def atb(self, data):
+        if self.root == None:
+            new_node = Node(data)
+            self.root = new_node
