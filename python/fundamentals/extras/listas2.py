6c5ba63b43ee52aeaad6b9df065334da044daac9
CyKl0P5 <ignaciofloreslarranaga@alumnos.sip.cl>
2023-04-17 14:13:22 -0400

    zxvdfv

--- python/fundamentals/extras/listas2.py
+++ python/fundamentals/extras/listas2.py
@@ -4,18 +4,18 @@ class Node:
         self.next = None
         self.prev = None
 
-class Elist:
+
+class DList:
     def __init__(self):
         self.root = None
 
-    def ilv(self, data): 
+    def ilv(self, data):  
         if self.root == None:
             new_node = Node(data)
             self.root = new_node
-        else: 
-            print
+        else:  
+            print("La lista no esta vacia")
         return self
-
 
     def atf(self, data):
         if self.root == None:
@@ -29,7 +29,7 @@ class Elist:
         return self
 
     def atb(self, data):
-        if self.root == None:
+        if self.root == None: 
             new_node = Node(data)
             self.root = new_node
             return
@@ -40,7 +40,7 @@ class Elist:
         runner.next = new_node
         new_node.prev = runner
         return self
-
+    
     def ide(self, n, data):
         if self.root == None:
             print("L/V")
@@ -82,7 +82,7 @@ class Elist:
             runner = runner.next
         runner.prev.next = None
         return self
-
+    
     def debValued(self, val):
         if self.root == None:
             print("L/V")
@@ -98,5 +98,71 @@ class Elist:
             if runner.value == val:
                 break
             runner = runner.next
+        if runner.next != None:
+            runner.prev.next = runner.next
+            runner.next.prev = runner.prev
+        else:
+            if runner.value == val:
+                self.delB()
+            else:
+                print("N/F")
+        print(runner.value, "uwu")
+        return self
+
+    def print_info(self):
+        if self.root == None:
+            print("L/V")
+            return
+        else:
+            runner = self.root
+            while runner != None:
+                print(runner.value, " ")
+                runner = runner.next
+        return self
+
+    def LCircular(self):
+        runner = self.root
+        while runner.next != None:
+            runner = runner.next
+        if runner.next == None:
+            print("No es circular")
+        else:
+            print("Es circular")
+        return self
+
+    def mitad(self):
+        runner = self.root
+        count = 0
+        while runner != None:
+            count += 1
+            runner = runner.next
+        runner_two = self.root
+        count = count / 2
+        b = 0
+        while b < count:
+            print(runner_two.value)
+            runner_two = runner_two.next
+            b += 1
+        return self
 
 
+test = DList()
+test.ilv(4)
+test.atb(8).atf(1).atf(4).atb(2).atb(5).atf(3).atb(6)
+test.print_info()
+print("--------------------------------------------------")
+test.delI()
+test.print_info()
+print("--------------------------------------------------")
+test.delB()
+test.print_info()
+print("--------------------------------------------------")
+test.debValued(8)
+test.print_info()
+print("--------------------------------------------------")
+test.ide(1, 78)
+test.print_info()
+print("--------------------------------------------------")
+test.countE()
+print("--------------------------------------------------")
+test.mitad()
